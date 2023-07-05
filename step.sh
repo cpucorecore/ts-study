cf32f71eb4e4512994b8989e0c45ca2f9b3c3b23
sky <sky@skydeMac-mini.local>
2023-07-05 21:06:38 +0800

    add ts-node usage

--- step.sh
+++ step.sh
@@ -1,4 +1,9 @@
+# init a typescript project
 npm init
 npm install --save-dev typescript tslint @types/node
 npx tslint --init
 npm install --save qheap
+
+# use ts-node
+npm i -D ts-node
+npx ts-node src/index.ts
